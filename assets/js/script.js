3dff9249eb0704d62035d1321bcc6b0dae1c4f5b
Daniel Lewis <daniel.lewis@protonmail.com>
2021-02-12 22:21:05 -0700

    added weak password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 // Assignment code here
-
-
+function generatePassword() {
+  // terrible generator
+  let r = Math.random().toString(36).substring(5);
+  return r;
+  }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
